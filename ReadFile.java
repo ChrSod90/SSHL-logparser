56208efc7c4b04996d6b2fca76e4391843182a85
ChrSod90 <Christoffer.Soderstrom.8799@student.uu.se>
2020-03-06 18:11:34 +0100

    regex testing

--- ReadFile.java
+++ ReadFile.java
@@ -3,25 +3,38 @@ import java.io.FileNotFoundException;
 import java.util.Scanner; 
 import java.io.FileWriter; 
 import java.io.IOException; 
+import java.util.regex.*;
+
 
 public class ReadFile {
-  public static void main(String[] args) {
-    try {
-      File myObj = new File(args[0]);
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        if (data.contains("<timestamp>")) {
-          System.out.println(data);
+    public static void main(String[] args) {
+        String date = "0";
+        try {
+            File myObj = new File(args[0]);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                
+                if (data.contains("<timestamp>")) {
+                    date = data;
+                }
+                if (data.contains("<description>Giltigt kort ")) {
+                    System.out.println(date);
+                    System.out.println(data);
+                    Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(data);
+                    while(m.find()) {
+                        System.out.println("employee ID: " + m.group(1));    
+                    }
+                    System.out.println("");
+                                       
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
-        if (data.contains("<description>Giltigt kort ")) {
-          System.out.println(data);
-        }
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
     }
-  }
 }
+
+
