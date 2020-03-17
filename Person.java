33a1b0bd71b94092b059b0aae4422333d0c3e02d
ChrSod90 <Christoffer.Soderstrom.8799@student.uu.se>
2020-03-17 17:36:13 +0100

    started on person class

---
+++ Person.java
@@ -0,0 +1,15 @@
+package person;
+
+public class Person{
+
+    private String name;
+    private int id;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setID(String id){
+        this.id = integer.parseInt(id);
+    }
+        
+}
